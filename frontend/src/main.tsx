66cfd94d3d47b199444c74f7a841778b5bea7faf
sathiyaseelank-dot <sathiyaseelank326@gmail.com>
2026-05-07 17:22:39 +0530

    second commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  ApolloClient, 
+  ApolloProvider, 
+  InMemoryCache,
+} from "@apollo/client"
+
 import './index.css'
 import App from './App.tsx'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8083/query',
+  cache: new InMemoryCache(),
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
