feaff351795f89769e26f0321b9e0330e03c85a7
sathiyaseelank-dot <sathiyaseelank326@gmail.com>
2026-05-07 17:11:11 +0530

    first commit

---
+++ backend/database.go
@@ -0,0 +1,20 @@
+package main 
+
+import(
+	"database/sql"
+	"log"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func ConnectDB() *sql.DB {
+	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=graphql_demo sslmode=disable"
+	 
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("database connected")
+	return db
+}
